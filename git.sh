89003480c03778524db8ecf1d998b4c5545a1dd7
RicardoFW11 <ricardo.fajardo@cgcompass.com>
2025-10-20 23:51:27 +0000

    repository imported

--- git.sh
+++ git.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Configurar automáticamente la cuenta de Git para RicardoFW11
+echo "Configuring Git account for RicardoFW11..."
+git config user.name "RicardoFW11"
+git config user.email "ricardo.fajardo@cgcompass.com"
+
+# Verificar que el remote esté configurado correctamente (HTTPS en lugar de SSH)
+current_remote=$(git remote get-url origin)
+if [[ $current_remote == git@github.com:* ]]; then
+    echo "Converting SSH remote to HTTPS..."
+    repo_path=$(echo $current_remote | sed 's/git@github.com://' | sed 's/.git$//')
+    git remote set-url origin "https://github.com/$repo_path.git"
+fi
+
+echo "Git configuration updated successfully!"
+echo ""
+
 # Pregunta por el comentario del commit
 echo "Enter the commit message:"
 read commitMessage
